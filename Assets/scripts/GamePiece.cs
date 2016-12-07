43efcf7d8c65da43cdbf747c3d29a4bb3240a571
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-07 20:10:55 +0000

    Create GamePiece prefabs and class

---
+++ Assets/scripts/GamePiece.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GamePiece : MonoBehaviour {
+
+	private int xIndex;
+	private int yIndex;
+
+	public void SetCoord(int x, int y) {
+		xIndex = x;
+		yIndex = y;
+	}
+}
