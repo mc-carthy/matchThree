6e949c6dd5a19a8c1871c01853f454754420a647
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-07 19:10:34 +0000

    Instantiate board

---
+++ Assets/scripts/Board.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject tilePrefab;
+	[SerializeField]
+	private int height;
+	[SerializeField]
+	private int width;
+
+	private Tile[,] allTiles;
+
+	private void Start () {
+		allTiles = new Tile[width, height];
+		SetupTiles();
+	}
+
+	private void SetupTiles () {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				GameObject newTile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+				newTile.name = "Tile (" + i + ", " + j + ")";
+				newTile.transform.parent = transform;
+				allTiles[i, j] = newTile.GetComponent<Tile>();
+			}
+		}
+	}
+}
