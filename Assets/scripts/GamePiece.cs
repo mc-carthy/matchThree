33aa43ba675481bd45de1a459d5f60c2153c1e78
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-07 20:48:35 +0000

    Add easing function to GamePiece movement

--- Assets/scripts/GamePiece.cs
+++ Assets/scripts/GamePiece.cs
@@ -6,13 +6,24 @@ public class GamePiece : MonoBehaviour {
 	private int xIndex;
 	private int yIndex;
 	private bool isMoving;
+	[Range(0, 2)]
+	private float easeAmount = 1.5f;
 
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			Move(xIndex - 1, yIndex);
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			Move(xIndex + 1, yIndex);
+		}
+	}
+	
 	public void SetCoord (int x, int y) {
 		xIndex = x;
 		yIndex = y;
 	}
 
-	public void Move (int destX, int destY, float timeToMove) {
+	public void Move (int destX, int destY, float timeToMove = 0.5f) {
 		if (!isMoving) {
 			StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
 		}
@@ -31,10 +42,18 @@ public class GamePiece : MonoBehaviour {
 				break;
 			}
 			elapsedTime += Time.deltaTime;
-			float t = Mathf.Clamp(elapsedTime / timeToMove, 0, 1);
+			float t = Mathf.Clamp01(elapsedTime / timeToMove);
+			t = Ease(t);
+
+
 			transform.position = Vector3.Lerp(startPos, destination, t);
 			yield return null;
 		}
 		isMoving = false;
+	}
+
+	private float Ease (float x) {
+		float a = easeAmount + 1;
+		return Mathf.Pow(x, a) / (Mathf.Pow(x, a) + Mathf.Pow(1 - x, a));
 	}
 }
