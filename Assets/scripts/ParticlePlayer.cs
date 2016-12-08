b31435180313e672f1d4d280fb45fd96b5fcd37b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-08 18:59:26 +0000

    Add ParticlePlayer class

---
+++ Assets/scripts/ParticlePlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ParticlePlayer : MonoBehaviour {
+
+	[SerializeField]
+	private ParticleSystem[] allParticles;
+	private float lifeTime = 1f;
+
+	private void Start () {
+		allParticles = GetComponentsInChildren<ParticleSystem>();
+
+		Destroy(gameObject, lifeTime);
+	}
+
+	public void Play () {
+		foreach (ParticleSystem ps in allParticles) {
+			ps.Stop();
+			ps.Play();
+		}
+	}
+}
