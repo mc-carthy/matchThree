270126049955de55c36138b65084ecff2dd3745f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-09 14:15:46 +0000

    Collectibles are destroyed upon hitting the bottom row

--- Assets/scripts/Board.cs
+++ Assets/scripts/Board.cs
@@ -719,6 +719,8 @@ public class Board : MonoBehaviour {
 			yield return new WaitForSeconds(0.2f);
 
 			matches = FindMatchesAt(movingPieces);
+			collectedPieces = FindCollectiblesAt(0);
+			matches = matches.Union(collectedPieces).ToList();
 
 			if (matches.Count == 0) {
 				isFinished = true;
